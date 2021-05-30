ff68f93f24d80721b31bea10a8e8a3f1dc42f3fc
Mohsin222 <50231604+Mohsin222@users.noreply.github.com>
2021-05-30 20:54:44 +0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,25 @@
+package a1;
+
+
+public class Car {
+
+	public Car() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		bike obj = new bike();
+		obj.BikeCalssObj();
+
+	}
+
+}
+
+class  bike{
+  void BikeCalssObj() {
+	  System.out.println("this is byke");
+  }
+	
+	
+}
